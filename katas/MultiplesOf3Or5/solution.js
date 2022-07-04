9f5554634f7b38a520799926e56f4c87ff263e8d
Volad <artosan360@gmail.com>
2022-07-04 15:48:35 +0500

    refactored

--- katas/MultiplesOf3Or5/solution.js
+++ katas/MultiplesOf3Or5/solution.js
@@ -1,8 +1,11 @@
 export const solution = (numberString) => {
   let number = Number(numberString);
   let set = new Set();
-  for (let n = 3; n < number; n++) {
-    if ((n % 3 === 0 || n % 5 === 0) && n > 0) set.add(n);
+  for (let n = 3; n < number; n += 3) {
+    set.add(n);
+  }
+  for (let n = 5; n < number; n += 5) {
+    set.add(n);
   }
   return set.size > 0 ? Array.from(set).reduce((p, c) => p + c) : 0;
 };
