fc92bbeaeda45a25afa43448513c939e9ad6c144
Volad <artosan360@gmail.com>
2022-07-09 03:28:35 +0500

    connected values need fix 1

--- katas/ConnectingValues/solution.js
+++ katas/ConnectingValues/solution.js
@@ -1,13 +1,60 @@
-export const connectedValues = (arr, val, coord) => {};
-export const directionName = {
-  top: "top",
-  topRight: "topRight",
-  right: "right",
-  bottomRight: "bottomRight",
-  bottom: "bottom",
-  bottomLeft: "bottomLeft",
-  left: "left",
-  topLeft: "topLeft",
+export const directionInfo = {
+  top: {
+    name: "top",
+    moveOn: {
+      x: 0,
+      y: -1,
+    },
+  },
+  topRight: {
+    name: "topRight",
+    moveOn: {
+      x: 1,
+      y: -1,
+    },
+  },
+  right: {
+    name: "right",
+    moveOn: {
+      x: 1,
+      y: 0,
+    },
+  },
+  bottomRight: {
+    name: "bottomRight",
+    moveOn: {
+      x: 1,
+      y: 1,
+    },
+  },
+  bottom: {
+    name: "bottom",
+    moveOn: {
+      x: 0,
+      y: 1,
+    },
+  },
+  bottomLeft: {
+    name: "bottomLeft",
+    moveOn: {
+      x: -1,
+      y: 1,
+    },
+  },
+  left: {
+    name: "left",
+    moveOn: {
+      x: -1,
+      y: 0,
+    },
+  },
+  topLeft: {
+    name: "topLeft",
+    moveOn: {
+      x: -1,
+      y: -1,
+    },
+  },
 };
 
 export const getDirectionsAbleForMove = (arrSize, coord) => {
@@ -18,28 +65,63 @@ export const getDirectionsAbleForMove = (arrSize, coord) => {
     left: coord.x > 0,
   };
   const moveIsAble = [
-    { direction: directionName.top, able: moveCheck.top },
+    { direction: directionInfo.top, able: moveCheck.top },
     {
-      direction: directionName.topRight,
+      direction: directionInfo.topRight,
       able: moveCheck.top && moveCheck.right,
     },
-    { direction: directionName.right, able: moveCheck.right },
+    { direction: directionInfo.right, able: moveCheck.right },
     {
-      direction: directionName.bottomRight,
+      direction: directionInfo.bottomRight,
       able: moveCheck.bottom && moveCheck.right,
     },
-    { direction: directionName.bottom, able: moveCheck.bottom },
+    { direction: directionInfo.bottom, able: moveCheck.bottom },
     {
-      direction: directionName.bottomLeft,
+      direction: directionInfo.bottomLeft,
       able: moveCheck.bottom && moveCheck.left,
     },
-    { direction: directionName.left, able: moveCheck.left },
+    { direction: directionInfo.left, able: moveCheck.left },
     {
-      direction: directionName.topLeft,
+      direction: directionInfo.topLeft,
       able: moveCheck.top && moveCheck.left,
     },
   ];
-  return moveIsAble
-    .filter((value) => value.able === true)
-    .map((value) => value.direction);
+  let withFilter = moveIsAble.filter((value) => value.able === true);
+  if (withFilter.length > 0) {
+    return withFilter.map((value) => value.direction);
+  } else {
+    return null;
+  }
+};
+
+export const connectedValues = (arr, val, coord) => {
+  let history = [];
+  const moveThroghConnectedValues = (arr, val, coord) => {
+    let coordX = coord[1];
+    let coordY = coord[0];
+    let directions = getDirectionsAbleForMove(
+      { width: arr[0].length, height: arr.length },
+      { x: coordX, y: coordY }
+    ).filter((direction) => {
+      return (
+        arr[coordY + direction.moveOn.y][coordX + direction.moveOn.x] === val
+      );
+    });
+    if (directions != null) {
+      directions.forEach((direction) => {
+        let newCoordY = coordY + direction.moveOn.y;
+        let newCoordX = coordX + direction.moveOn.x;
+        if (
+          history.every(
+            (oldCoord) => oldCoord.toString() != [newCoordY, newCoordX]
+          )
+        ) {
+          history.push([coordY, coordX]);
+          return moveThroghConnectedValues(arr, val, [newCoordY, newCoordX]);
+        }
+      });
+    }
+  };
+  moveThroghConnectedValues(arr, val, coord);
+  return history;
 };
--- katas/ConnectingValues/test.js
+++ katas/ConnectingValues/test.js
@@ -1,32 +1,51 @@
 import assert from "assert/strict";
-import { getDirectionsAbleForMove, directionName } from "./solution.js";
+import {
+  getDirectionsAbleForMove,
+  directionInfo,
+  connectedValues,
+} from "./solution.js";
 
 describe("Helpfull methods Tests", function () {
   it("3x3 array without left directions", function () {
     assert.deepEqual(
-      getDirectionsAbleForMove({ width: 3, height: 3 }, { x: 0, y: 1 }),
+      getDirectionsAbleForMove({ width: 3, height: 3 }, { x: 0, y: 1 }).map(
+        (value) => value.name
+      ),
       [
-        directionName.top,
-        directionName.topRight,
-        directionName.right,
-        directionName.bottomRight,
-        directionName.bottom,
+        directionInfo.top.name,
+        directionInfo.topRight.name,
+        directionInfo.right.name,
+        directionInfo.bottomRight.name,
+        directionInfo.bottom.name,
       ]
     );
   });
   it("3x3 array with all directions", function () {
     assert.deepEqual(
-      getDirectionsAbleForMove({ width: 3, height: 3 }, { x: 1, y: 1 }),
+      getDirectionsAbleForMove({ width: 3, height: 3 }, { x: 1, y: 1 }).map(
+        (value) => value.name
+      ),
       [
-        directionName.top,
-        directionName.topRight,
-        directionName.right,
-        directionName.bottomRight,
-        directionName.bottom,
-        directionName.bottomLeft,
-        directionName.left,
-        directionName.topLeft,
+        directionInfo.top.name,
+        directionInfo.topRight.name,
+        directionInfo.right.name,
+        directionInfo.bottomRight.name,
+        directionInfo.bottom.name,
+        directionInfo.bottomLeft.name,
+        directionInfo.left.name,
+        directionInfo.topLeft.name,
       ]
     );
+  });
+});
+
+describe("ConnectingValues", () => {
+  it("Test", () => {
+    let exampleArr = [
+      [1, 0, 2, 0, 2, 1],
+      [1, 0, 2, 1, 5, 7],
+      [4, 1, 1, 0, 1, 9],
+    ];
+    assert.strictEqual(connectedValues(exampleArr, 1, [0, 0]), 0);
   });
 });
