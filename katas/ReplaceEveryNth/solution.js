7eda767343093cf28d2574d20d43de2df11ead46
Volad <artosan360@gmail.com>
2022-07-12 16:25:39 +0500

    added jscod signatures

--- katas/ReplaceEveryNth/solution.js
+++ katas/ReplaceEveryNth/solution.js
@@ -1,8 +1,16 @@
+/**
+ * @param  {string} text
+ * @param  {number} n
+ * @param  {string} oldValue
+ * @param  {string} newValue
+ */
 export const replaceNth = (text, n, oldValue, newValue) => {
-    let nth = 0;
-    return text.split("").reduce((previousValue, currentValue) => {
-        let equality = currentValue === oldValue || previousValue === oldValue;
-        if (equality) nth++;
-        return equality && nth % n === 0 && n > -1 ? previousValue + newValue : previousValue + currentValue;
-    })
+  let nth = 0;
+  return text.split("").reduce((previousValue, currentValue) => {
+    let equality = currentValue === oldValue || previousValue === oldValue;
+    if (equality) nth++;
+    return equality && nth % n === 0 && n > -1
+      ? previousValue + newValue
+      : previousValue + currentValue;
+  });
 };
