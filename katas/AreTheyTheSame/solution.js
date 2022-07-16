a4b5e6737fcd31b70fbc1a5047fe471421f22f04
Volad <artosan360@gmail.com>
2022-07-16 17:26:06 +0500

    setuped eslint

--- katas/AreTheyTheSame/solution.js
+++ katas/AreTheyTheSame/solution.js
@@ -1,5 +1,5 @@
 export const comp = (array1, array2) => {
-  //select iterately item from array1
+  // select iterately item from array1
 
   // array1.forEach((value1) => {
   //   let finded = array2.filter((value2, index) => {
@@ -18,9 +18,11 @@ export const comp = (array1, array2) => {
     (value) => array2.includes(value) || array2.includes(value * value)
   );
 
-  //find in array2 equal item OR square of this item
-  //if item finded remove it from array2
+  // find in array2 equal item OR square of this item
+  // if item finded remove it from array2
   //  go to the next iterately item
   //  if theere is no next items then return true
-  //else return false that these arrays are not equal
+  // else return false that these arrays are not equal
 };
+
+export default comp;
--- katas/AreTheyTheSame/test.js
+++ katas/AreTheyTheSame/test.js
@@ -1,10 +1,10 @@
 import assert from "assert/strict";
-import { comp } from "./solution.js";
+import { comp } from "./solution";
 
 describe("Tests", () => {
   it("test", () => {
-    let a1 = [121, 144, 19, 161, 19, 144, 19, 11];
-    let a2 = [
+    const a1 = [121, 144, 19, 161, 19, 144, 19, 11];
+    const a2 = [
       11 * 11,
       121 * 121,
       144 * 144,
@@ -17,8 +17,8 @@ describe("Tests", () => {
     assert.strictEqual(comp(a1, a2), true);
   });
   it("random test", () => {
-    let a1 = [1, 2, 5, 3, 3, 6, 10, 9, 2, 8, 0, 5, 7, 0, 2, 4, 9];
-    let a2 = [0, 9, 9, 64, 1, 25, 4, 81, 4, 16, 49, 4, 1, 25, 100, 36, 81];
+    const a1 = [1, 2, 5, 3, 3, 6, 10, 9, 2, 8, 0, 5, 7, 0, 2, 4, 9];
+    const a2 = [0, 9, 9, 64, 1, 25, 4, 81, 4, 16, 49, 4, 1, 25, 100, 36, 81];
     assert.strictEqual(comp(a1, a2), false);
   });
 });
